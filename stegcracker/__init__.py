c4166ca4c444dda011a68e0a6b6237049fe34a42
Paradoxis <luke@paradoxis.nl>
2019-05-02 13:29:45 +0200

    Bumped version

--- stegcracker/__init__.py
+++ stegcracker/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '2.0.6'
+__version__ = '2.0.7'
 __description__ = 'Steganography brute-force utility to uncover hidden data inside files'
 __url__ = 'https://github.com/Paradoxis/StegCracker'
